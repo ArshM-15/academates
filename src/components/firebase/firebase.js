d4e7dc1045fce6ae1b8e65353730f94d74c80eb1
Arshvir Mundi <arshvir.mundi@gmail.com>
2023-07-26 17:29:54 -0400

    added .env to .gitignore

--- src/components/firebase/firebase.js
+++ src/components/firebase/firebase.js
@@ -5,8 +5,9 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-   apiKey: `${process.env.REACT_APP_API_KEY}`,
+  apiKey: `${process.env.REACT_APP_API_KEY}`,
   authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
   projectId: "academates-53fe5",
   storageBucket: "academates-53fe5.appspot.com",
